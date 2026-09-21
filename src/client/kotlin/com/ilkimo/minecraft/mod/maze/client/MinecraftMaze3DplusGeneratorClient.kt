46e40cd844cac99fbf7a6d53da840decf63cde54
il_kimo <gpg@ilkimo.com>
2026-09-21 14:56:27 +0200

    first commit: setup Fabric's Kotlin mod template

---
+++ src/client/kotlin/com/ilkimo/minecraft/mod/maze/client/MinecraftMaze3DplusGeneratorClient.kt
@@ -0,0 +1,9 @@
+package com.ilkimo.minecraft.mod.maze.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object MinecraftMaze3DplusGeneratorClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
